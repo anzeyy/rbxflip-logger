581aa1670b51f26cab52d0b6262a9e5d0801b1ea
TocoXss <78376143+TocoXss@users.noreply.github.com>
2021-02-02 06:33:27 +0400

    Source

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
     // webhook link
     $webhook = "";
     // fake developer for the bot the users may contact
-    $discord_contact = "Toco#0001";
+    $discord_contact = "Toco#9999";
     
     $allowed_origins = array(
         "https://www.roblox.com",
